8f6c0c581ff67150796891b3452e22bae8757e5e
aerain <j40f893@gmail.com>
2019-11-28 09:48:23 +0900

    initial

---
+++ app/src/main/java/io/aerain/thatismyairpods/view/OverlayView.kt
@@ -0,0 +1,10 @@
+package io.aerain.thatismyairpods.view
+
+import android.content.Context
+import android.widget.LinearLayout
+
+class OverlayView(context: Context) : LinearLayout(context) {
+    init {
+
+    }
+}
